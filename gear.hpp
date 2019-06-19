06cbc6e602d7f9959bb09959747d87103d21779f
exesaLigno <cfg888powerfull@gmail.com>
2019-06-19 21:14:07 +0300

    gear library

---
+++ gear.hpp
@@ -0,0 +1,54 @@
+/*	
+ *
+ *	implement createConnection with execl
+ *
+ *
+ *
+ */
+
+class Gear
+{
+private:
+	char deviceAdress[22] = "0.0.0.0:26101";
+	
+public:
+	Gear(char adress[]);
+	int createConnection();
+	int fileTransferToGear(char initial_file_path[], char target_file_path[]); // Not implemented
+	int fileTransferFromGear(char initial_file_path[], char target_file_path[]); // Not implemented
+	~Gear();
+};
+
+
+
+Gear::Gear(char adress[])
+{
+    strcpy(deviceAdress, adress);
+    strcat(deviceAdress, ':');
+	strcat(deviceAdress, "26101");
+}
+
+Gear::~Gear()
+{}
+
+int Gear::createConnection()
+{
+	char command[34] = "adb connect ";
+	strcat(command, deviceAdress);
+	//execl("/usr/bin/fish/", "adb", "connect", deviceAdress, NULL);
+	system(command);
+	return 0;
+}
+
+int Gear::refuseConnection()
+{
+    char command[37] = "adb disconnect ";
+	strcat(command, deviceAdress);
+	system(command);
+	return 0;
+}
+
+int Gear::fileTransferToGear(char initial[], char target[] = "")
+{
+    return 0;
+}
